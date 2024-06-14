d280bafe9608c747ef333b2a21e3f4cacef787d3
shreyxnsh <shreyanshatwork@gmail.com>
2024-06-15 00:02:23 +0530

    theme files

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -4,7 +4,7 @@ class FColors {
   // App theme colors
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
-  static const Color accent = Color(0xFFb0c7ff);
+  static const Color accent = Color(0xFF249781);
 
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
@@ -13,7 +13,7 @@ class FColors {
 
   // Background colors
   static const Color light = Color(0xFFF6F6F6);
-  static const Color dark = Color(0xFF272727);
+  static const Color dark = Color(0xFF0a151b);
   static const Color primaryBackground = Color(0xFFF3F5FF);
 
   // Background Container colors
