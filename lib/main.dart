9aeeca5b3ddb05c9fe74e2704433b78b0900322f
shreyxnsh <shreyanshatwork@gmail.com>
2024-06-17 00:04:22 +0530

    main.dart fixed for camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+
+import 'package:camera/camera.dart';
+import 'package:chatsy/features/camera/screens/camera.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const App());
 }
 
